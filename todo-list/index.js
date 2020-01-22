5455e49b1f2f334c72f9f455433604765c335fca
ljh <ljh@flasystem.com>
2020-01-22 18:09:40 +0900

    font-awesome + reset css

---
+++ todo-list/index.js
@@ -0,0 +1,20 @@
+// console.log("Im Working. Im JS. Im Beatiful");
+/*
+const a = 221;
+let b = a - 5;
+a = 4;
+console.log(b, a);
+*/
+/*
+// String
+const what = "YOLO.JIHWAN";
+console.log(what);
+*/
+/*
+Boolean
+const wat = true;
+*/
+/*
+Number
+const wat = 44444;
+*/
