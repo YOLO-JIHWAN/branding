c6b606d32df67f2b7ca0a160e721ed951afb4ae1
YOLO-JIHWAN <yolo.jihwan@gmail.com>
2020-04-22 17:13:12 +0900

    custom-web-clock

--- portfolio/project-list/javascript/custom-web-home/common.js
+++ portfolio/project-list/javascript/custom-web-home/common.js
@@ -1,17 +1,68 @@
-//
+//clock
 const clockContainer = document.querySelector(".js-clock"),
-    clockTitle = clockContainer.querySelector("h1");
+    clockTitle = clockContainer.querySelector("strong"),
+    clockAmPm = clockContainer.querySelector("span");
 
 function getTime () {
     const date = new Date();
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+    /*
+    if(hours >= 12) {
+        clockAmPm = "PM";
+        hours = (date.getHours() - 2);
+    }
+    */
+    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
 }
+
+//greeting
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing"; //CN = Class Name
+
+function saveName(text) {
+    localStorage.setItem(USER_LS, text)
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text) {
+    form.classList.remove(SHOWING_CN);
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
+}
+
+//local storage
+function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+    if(currentUser === null) {
+        //not exist user
+        askForName();
+    } else {
+        paintGreeting(currentUser);
+    }
+}
+
 
 function init() {
     getTime();
+    setInterval(getTime, 1000);
+    loadName();
 }
 
 init();
